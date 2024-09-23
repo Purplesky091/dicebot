736cebd41ffb1fc88f738fd88c68a5ced4e028a3
purplesky091 <ray.hsia@gmail.com>
2024-09-23 00:55:48 -0500

    basic implementation. Code structuring will come next

--- dice.go
+++ dice.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func generateNumber(min int, max int) (result int) {
@@ -11,12 +14,32 @@ func generateNumber(min int, max int) (result int) {
 }
 
 func main() {
+	var max int
 	var dice string
+	var sum int
+
 	fmt.Print("Input dice: ")
 	fmt.Scan(&dice)
 
 	r, _ := regexp.Compile(`^(\d*)d(\d+)$`)
 	var IsValid bool = r.MatchString(dice)
-	fmt.Printf("%s is valid: %t", dice, IsValid)
-	// fmt.Printf("Printing random number between 1 - %d: %d\n", upperBound, generateNumber(1, upperBound))
+	fmt.Printf("%s is valid: %t\n", dice, IsValid)
+	if !IsValid {
+		os.Exit(1)
+	}
+
+	// parsing dice
+	DiceValues := strings.Split(dice, "d")
+	numOfRolls, _ := strconv.Atoi(DiceValues[0])         // can use _ because I verified before the second value is an integer.
+	max, _ = strconv.Atoi(DiceValues[len(DiceValues)-1]) // can use _ because I verified before the second value is an integer.
+
+	rolls := make([]int, numOfRolls)
+	fmt.Printf("Rolling dice %s.\n", dice)
+	for i := 0; i < numOfRolls; i++ {
+		rolls[i] = generateNumber(1, max)
+		fmt.Printf("Roll %d: %d\n", i+1, rolls[i])
+		sum += rolls[i]
+	}
+
+	fmt.Printf("Sum: %d", sum)
 }
